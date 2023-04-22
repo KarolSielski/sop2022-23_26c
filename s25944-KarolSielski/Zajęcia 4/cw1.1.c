00e2ac2885c1a562be47ba88f9d5cb381b9823d2
KarolSielski02 <mrkarlos2002@gmail.com>
2023-04-22 13:23:27 +0200

    added 1st C exrcs

---
+++ s25944-KarolSielski/Zajęcia 4/cw1.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int number, sum = 0;
+do{
+    number = 0;
+    printf("Podaj liczbe: ");
+    scanf("%d", &number);
+    sum += number;
+} while (number != 0);
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
